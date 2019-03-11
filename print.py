4762ce8b96296d1e3ac18c9a68760c81b2cd374f
Rizwanjamal <Rizwan_hassanfatah@hotmail.com>
2019-03-11 20:52:44 +0500

    Getting user input in python

--- print.py
+++ print.py
@@ -1,2 +1,6 @@
 print("Languages: \nPython\nRuby\nJavascript")
 print("Languages: \n\tPython\n\tRuby\n\tJavascript")
+
+print("Hello", "Rizwan", "Jamal", end="\t => ")
+
+print("PYTHON", "TOPIC", "PRINT", sep=" -> ")
---
+++ user_input.py
@@ -0,0 +1,4 @@
+# Getting Value From User
+
+cityName = input("Enter Name of the city ?");
+print(cityName);
